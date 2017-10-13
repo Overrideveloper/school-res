369984cbd3c96c01ae5b7dc5b4a7d4e71fef97fa
overrideveloper <bansowisdom@gmail.com>
2017-10-13 04:37:02 +0100

    Added student list component

---
+++ src/components/StudentList.vue
@@ -0,0 +1,25 @@
+<template>
+ <div></div>
+</template>
+
+<script>
+export default {
+  name: 'StudentList',
+  data() {
+    return {
+      header: 'All Students',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  font-weight: bold;
+  font-family: Tahoma;
+}
+span {
+  font-family: Tahoma;
+}
+</style>
