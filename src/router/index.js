8e9f91af057c2e5f640f73d15067d3276026bb0e
overrideveloper <bansowisdom@gmail.com>
2017-10-17 07:08:12 +0100

    Removed AddStudent component and route

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import LandingPage from '../components/LandingPage';
 import StudentList from '../components/StudentList';
-import AddStudent from '../components/AddStudent';
 
 Vue.use(Router);
 
@@ -19,10 +18,6 @@ export default new Router({
     {
       path: '/all_students',
       component: StudentList,
-    },
-    {
-      path: '/add_student',
-      component: AddStudent,
     },
   ],
 });
